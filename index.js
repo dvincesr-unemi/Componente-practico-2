46ac925a97ab6ccab4939642dc542fae91513c24
derickvinces87-hub <derickvinces87@gmail.com>
2025-11-16 21:21:38 -0500

    Mejoré mensajes

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function promedio() {
-    let cantidad =parseInt(prompt("¿Cuántos números deseas promediar?"));
-    if (cantidad <= 0 || isNaN(cantidad)) {
-        alert("Por favor ingresa un número válido mayor que cero.");
-    }else{
+    let cantidad =parseInt(prompt("¿Cuántos números deseas promediar?: "));
     let suma = 0;
+    let numerosIngresados=[];
     for (let i = 0; i < cantidad; i++) {
         let numero = parseFloat(prompt("Ingresa el número " + (i + 1) + ":"));
         suma += numero;
+        numerosIngresados.push(numero);
     }
     let promedio = suma / cantidad;
-    alert(promedio);
-    } 
-}     
+    alert("Numeros ingresados: "+numerosIngresados);
+    alert("Suma total: "+suma);
+    alert("EL promedio es: " + promedio);
+ }    
 promedio();
 
 
