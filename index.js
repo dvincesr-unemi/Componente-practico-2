661b87af622cc035991641398d050768355829ca
derickvinces87-hub <derickvinces87@gmail.com>
2025-11-16 19:02:27 -0500

    Primer commit

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function numerosPrimos(){
-    let numero=parseInt(prompt("Ingrese un número entero mayor que 1:"));                          
+function calcularPromedio(){
+    let cantidadNotas = parseInt(prompt("Ingrese la cantidad de notas a promediar:"));
+    let sumaNotas = 0;
+    for(let i = 0; i < cantidadNotas; i++){
+        let nota = parseFloat(prompt(`Ingrese la nota ${i + 1}:`));
+        sumaNotas += nota;
+    }
+    let promedio = sumaNotas / cantidadNotas;
+    alert(`El promedio de las notas es: ${promedio}`);
 }
+calcularPromedio();               
+
